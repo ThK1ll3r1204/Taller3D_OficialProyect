40188ae78ea936761f0a75db275e241bce272cdf
César Cadillo <gkktemporal@gmail.com>
2023-11-20 13:14:52 -0500

    Creación por grilla de 2X2

--- Proyecto3D_GH05TY/Assets/Scripts/GridScript.cs
+++ Proyecto3D_GH05TY/Assets/Scripts/GridScript.cs
@@ -17,62 +17,109 @@ public class GridScript : MonoBehaviour
     public bool BobElConstructor = true;
 
     [SerializeField]
-    int[,] grid = new int[10, 10];
-    int[,] grid2 = new int[2, 2];
-    [SerializeField]
     int[][,] arrayGridMatrix;
 
-    [SerializeField]
-    bool[] CanBuildThisBlock;
-
     [SerializeField] int[] g;
+
+    [SerializeField] int randomIndex;
+
+    int[,] grid1 = {  { 1, 1 },
+                      { 1, 1 } };
+
+    int[,] grid2 = { { 2, 1 },
+                     { 1, 1 } };
+
+    int[,] grid3 = { { 1, 1 },
+                     { 1, 1 } };
+
+    int[,] grid4 = { { 1, 2 },
+                     { 1, 1 } };
+
+    int[,] grid5 = { { 1, 2 },
+                     { 1, 0} };
+
+    int[,] grid6 = { { 1, 0 },
+                     { 1, 1 } };
+
+    int[,] grid7 = { { 3, 1 },
+                     { 1, 1 } };
+
+    int[,] grid8 = { { 2, 1 },
+                     { 1, 1 } };
+
+    int[,] grid9 = { { 1, 1 },
+                     { 1, 1 } };
     
-    [SerializeField] int h;
-    [SerializeField] int j;
+    int[,] grid10 = { { 1, 1 },
+                     { 1, 1 } };
 
     void Start()
     {
-        
+        arrayGridMatrix = new int[][,] { grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8, grid9, grid10 };
         BobElConstructor = true;
     }
 
     private void Update()
     {
-        h = Random.Range(1, g.Length + 1);
-        j = Random.Range(1, g.Length + 1);
-
-
-        int[,] gridx2 = { { j, j }, { j, j } };
-
-        int[,] grid ={
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h},
-        { h,h,h,h,h,h,h,h,h,h}};
-
-
-        arrayGridMatrix = new int[][,] { grid, gridx2 };
-
+        randomIndex = Random.Range(0, arrayGridMatrix.Length);
         if (BobElConstructor)
         {
-            StartCoroutine(SpawnCubes(grid));
+            StartCoroutine(SpawnCubes());
             BobElConstructor = false;
         }
         else
         {
             Debug.Log("NoEstaElConchaDeSuMadreDeBob");
         }
-
     }
+    
 
-    IEnumerator SpawnCubes(int[,] gridMatrix)
+    IEnumerator SpawnCubes()
     {
-        yield return new WaitForSeconds(1);
+        for (int y = 0; y < gridSize; y += 2)
+        {
+            for (int x = 0; x < gridSize; x += 2)
+            {
+                int[,] gridMatrix = arrayGridMatrix[randomIndex];
+                for (int i = 0; i < 2; i++)
+                {
+
+                    for (int j = 0; j < 2; j++)
+                    {
+
+                        int blockType = gridMatrix[i, j];
+                        Vector3 spawnPosition = new Vector3(x + j, 0, y + i) * 10;
+
+                        if (blockType == 1)
+                        {
+                            Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
+
+                        }
+                        else if (blockType == 2)
+                        {
+                            Instantiate(iceBlockPrefab, spawnPosition, Quaternion.identity);
+
+                        }
+                        else if (blockType == 3)
+                        {
+                            Instantiate(emptyBlockPrefab, spawnPosition, Quaternion.identity);
+
+                        }
+                        else if (blockType == 4)
+                        {
+                            Instantiate(MudBlockPrefab, spawnPosition, Quaternion.identity);
+
+                        }
+                        else
+                        {
+                            Instantiate(cubePrefab, spawnPosition, Quaternion.identity);                  
+
+                        }
+
+                        yield return new WaitForSeconds(delay);
+                    }
+                }
+            }
+        }
     }
 }
