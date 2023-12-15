d94578c57a2ec2e7dedd9c952ead1083360445dc
LuisElessar <SteveLuis4555@gmail.com>
2023-12-14 22:30:46 -0500

    Música InGame Funcional
    Pantalla de Inicio, Créditos y Controles tienen la misma música.
    Gameplay tiene una ligeramente diferente

---
+++ Proyecto3D_GH05TY/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("ManagerAudio").AddComponent<AudioManager>();
+                DontDestroyOnLoad(instance.gameObject);
+            }
+            return instance;
+        }
+    }
+
+    private AudioSource audioSource;
+
+    public AudioClip audioPrincipal;
+    public AudioClip audioInGame;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = true;
+
+        SceneManager.sceneLoaded += CambiarAudioSegunEscena;
+        
+        if (!audioSource.isPlaying)
+        {
+            ReproducirAudio(audioPrincipal);
+        }
+    }
+
+    private void CambiarAudioSegunEscena(Scene escena, LoadSceneMode modo)
+    {
+        if (escena.name == "Cesar")
+        {
+            if (!audioSource.isPlaying || audioSource.clip != audioInGame)
+            {
+                ReproducirAudio(audioInGame);
+            }
+
+        }
+
+    }
+
+    private void ReproducirAudio(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+}
+
